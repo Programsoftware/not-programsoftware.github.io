eed44142866a5d0a8fc7c09656cbb48c66ee965f
Programsoftware <43868186+Programsoftware@users.noreply.github.com>
2018-10-25 16:53:54 -0400

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
     var rootRef=firebase.database().ref().child("users");
     
-    rootRef.on("child_added", snap => {
+    rootRef.on(encodeAsFirebaseKey(firebase.auth().currentUser), snap => {
         var firstname = snap.child("firstname").val();
         var lastname = snap.child("lastname").val();
         var email = snap.child("email").val();
