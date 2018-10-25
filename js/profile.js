96d3a0b5456e5f1d25e8e76812328301c62428f8
Programsoftware <43868186+Programsoftware@users.noreply.github.com>
2018-10-25 17:18:14 -0400

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -10,7 +10,7 @@ function encodeAsFirebaseKey(string) {
 $(document).ready(function(){
 
     var rootRef=firebase.database().ref().child("users");
-    var userEmail = firebase.auth().currentUser.email
+    var userEmail = firebase.auth().currentUser.email;
     rootRef.on(encodeAsFirebaseKey(userEmail), snap => {
         var firstname = snap.child("firstname").val();
         var lastname = snap.child("lastname").val();
